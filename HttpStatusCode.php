f9d6d05b73a7de15d70674cf667d4fc58933ae13
Kamil Pietrzkiewicz <sargitcom@gmail.com>
2024-10-10 14:52:57 +0200

    feature: http snippet - simple list of general status codes

---
+++ HttpStatusCode.php
@@ -0,0 +1,8 @@
+class HttpStatusCode
+{
+    const RequestSuccess = 200; // former status code: OK
+    const BadRequest = 400;
+    const BadCredentials = 401;
+    const AccessDenied = 402;
+    const InternalServerError = 500;
+}
